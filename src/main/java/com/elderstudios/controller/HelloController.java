0367abc48b04550012853d871b72a44ae3a7f961
Adeel Mohammed <a.mohammed5832@student.leedsbeckett.ac.uk>
2017-04-27 17:07:48 +0100

    renamed

--- src/main/java/com/elderstudios/controller/HelloController.java
+++ src/main/java/com/elderstudios/controller/HelloController.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
 
@@ -19,6 +26,21 @@ public class HelloController {
 	private static final String GUESTBOOK_FORM = "guestbook";
 	private static final String ENTRIES_KEY = "entries";
 	private static final String FORM_KEY = "form";
+
+        private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+        private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	public HelloController()
+    {
+        reportCurrentTime();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String displayGuestbook( Model model ) {
